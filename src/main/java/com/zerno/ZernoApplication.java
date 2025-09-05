991e5f5972ed06cc05887596d2233bb926c18415
Yash Priyadarshan <yashpriyadarshan@gmail.com>
2025-09-05 23:30:58 +0530

    test

---
+++ src/main/java/com/zerno/ZernoApplication.java
@@ -0,0 +1,13 @@
+package com.zerno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZernoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZernoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zerno/ZernoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZernoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
